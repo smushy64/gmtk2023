570b7c40a4e874150f8dce693c1a352afb78b1a1
Alicia <smushyaa@gmail.com>
2023-07-08 16:56:27 -0400

    fixed itemobject null error

--- Assets/Scripts/Items/ItemObject.cs
+++ Assets/Scripts/Items/ItemObject.cs
@@ -4,16 +4,18 @@ namespace Items {
     [RequireComponent(typeof(SpriteRenderer))]
     public class ItemObject: MonoBehaviour {
         [SerializeField]
-        Sprite[] sprites = new Sprite[(int)Items.Item.MAX];
+        Sprite[] sprites = new Sprite[(int)Item.MAX];
 
         SpriteRenderer sprite_renderer;
 
         void Awake() {
-            sprite_renderer = GetComponent<SpriteRenderer>();
             sprite_renderer.sprite = sprites[0];
         }
 
         public void set_sprite( Item item ) {
+            if( !sprite_renderer ) {
+                sprite_renderer = GetComponent<SpriteRenderer>();
+            }
             sprite_renderer.sprite = sprites[(int)item];
         }
     }
