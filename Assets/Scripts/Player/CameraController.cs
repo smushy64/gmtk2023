38e2022ed29701731c2c7225f7f13645a453eb5c
Alicia <smushyaa@gmail.com>
2023-07-09 02:09:06 -0400

    final level background, collision, modified camera

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -1,31 +1,69 @@
-using System;
 using UnityEngine;
 
 namespace Player {
     public class CameraController: MonoBehaviour {
-        [SerializeField] private Transform trackingTransform;
-        [SerializeField] private Vector2 maxDistanceToMove;
-        [SerializeField] private Vector3 center;
+        [SerializeField]
+        Transform trackingTransform;
+        [SerializeField]
+        float minX, maxX;
+        [SerializeField]
+        float minY, maxY;
 
+        float aspect;
+        float half_size;
+
+        void Awake() {
+            Camera camera = GetComponent<Camera>();
+            half_size = camera.orthographicSize;
+            aspect = camera.aspect;
+        }
 
         private void Update() {
-            var playerOffset = this.trackingTransform.position - this.center;
+            float x = trackingTransform.position.x;
+            float y = trackingTransform.position.y;
 
-            if (playerOffset.x > this.maxDistanceToMove.x) {
-                playerOffset.x = this.maxDistanceToMove.x;
-            } else if (playerOffset.x < -this.maxDistanceToMove.x) {
-                playerOffset.x = -this.maxDistanceToMove.x;
-            }
-            if (playerOffset.y > this.maxDistanceToMove.y) {
-                playerOffset.y = this.maxDistanceToMove.y;
-            } else if (playerOffset.y < -this.maxDistanceToMove.y) {
-                playerOffset.y = -this.maxDistanceToMove.y;
+            float x_size = half_size * aspect;
+            if( x - x_size < minX ) {
+                x = minX + x_size;
+            } else if( x + x_size > maxX ) {
+                x = maxX - x_size;
             }
 
-            var moveTo = playerOffset;
+            if( y - half_size < minY ) {
+                y = minY + half_size;
+            } else if( y + half_size > maxY ) {
+                y = maxY - half_size;
+            }
 
-            moveTo.z = this.center.z;
-            this.transform.position = moveTo;
+            transform.position = new Vector3( x, y, -10.0f );
+        }
+
+        void OnDrawGizmosSelected() {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(
+                (Vector3.right * minX) +
+                (Vector3.down * 100.0f),
+                (Vector3.right * minX) +
+                (Vector3.up * 100.0f)
+            );
+            Gizmos.DrawLine(
+                (Vector3.right * maxX) +
+                (Vector3.down * 100.0f),
+                (Vector3.right * maxX) +
+                (Vector3.up * 100.0f)
+            );
+            Gizmos.DrawLine(
+                (Vector3.up * minY) +
+                (Vector3.left * 100f),
+                (Vector3.up * minY) + 
+                (Vector3.right * 100f)
+            );
+            Gizmos.DrawLine(
+                (Vector3.up * maxY) +
+                (Vector3.left * 100f),
+                (Vector3.up * maxY) + 
+                (Vector3.right * 100f)
+            );
         }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -61,6 +61,9 @@ namespace Player {
 
         public Action<int, int> on_health_update;
 
+        bool last_is_moving = false;
+        bool is_moving      = false;
+
         // NOTE(alicia): i-frames
         public bool is_invincible { private get; set; } = false;
         [SerializeField, Min(0.0f)]
@@ -105,6 +108,7 @@ namespace Player {
                 return;
             }
 
+            last_is_moving = is_moving;
             poll_input();
             if( player_input.is_interact_pressed ) {
                 if( held_item_type == Item.None ) {
@@ -185,16 +189,31 @@ namespace Player {
                 heldItem.gameObject.SetActive( false );
             }
 
-            if (Mathf.Approximately(this.player_input.movement.x, 0)) {
-                this.animator.SetInteger(ANIMATOR_HORIZONTAL_MOVE, 0);
+            // NOTE(alicia): mmm performance
+
+            if( !is_moving ) {
+                animator.Play( "Idle" );
             } else {
-                this.animator.SetInteger(ANIMATOR_HORIZONTAL_MOVE, this.player_input.movement.x >= 0 ? 1 : -1);
+                bool moving_vertically = 
+                    Mathf.Abs( player_input.movement.x ) <
+                    Mathf.Abs( player_input.movement.y );
+
+                if( moving_vertically ) {
+                    if( player_input.movement.y > 0f ) {
+                        animator.Play( "MoveUp" );
+                    } else {
+                        animator.Play( "MoveDown" );
+                    }
+                } else {
+                    if( player_input.movement.x < 0f ) {
+                        animator.Play( "MoveLeft" );
+
+                    } else {
+                        animator.Play( "MoveRight" );
+                    }
+                }
             }
-            if (Mathf.Approximately(this.player_input.movement.y, 0)) {
-                this.animator.SetInteger(ANIMATOR_VERTICAL_MOVE, 0);
-            } else {
-                this.animator.SetInteger(ANIMATOR_VERTICAL_MOVE, this.player_input.movement.y >= 0 ? 1 : -1);
-            }
+
         }
 
         private void FixedUpdate() {
@@ -245,16 +264,18 @@ namespace Player {
             float timer = 0f;
             float blink_timer = 0f;
             float blink_time = iFrameTime / 10.0f;
+            bool blinking = false;
             while( timer < iFrameTime ) {
                 timer += Time.deltaTime;
                 blink_timer += Time.deltaTime;
                 if( blink_timer >= blink_time ) {
-                    spriteRenderer.gameObject.SetActive( !spriteRenderer.gameObject.activeSelf );
+                    blinking = !blinking;
+                    spriteRenderer.color = blinking ? Color.clear : Color.white;
                     blink_timer = 0f;
                 }
                 yield return null;
             }
-            spriteRenderer.gameObject.SetActive( true );
+            spriteRenderer.color = Color.white;
             is_invincible = false;
         }
 
@@ -262,7 +283,6 @@ namespace Player {
             if( is_invincible ) {
                 return;
             }
-            Debug.Log( "Taking Damage: " + damage );
             health -= damage;
             is_invincible = true;
 
@@ -297,6 +317,8 @@ namespace Player {
                 mapped_input.Character.DiscardItem.WasPressedThisFrame(),
                 mapped_input.Character.DiscardItem.WasReleasedThisFrame()
             );
+
+            is_moving = player_input.movement.sqrMagnitude >= 0.001f;
         }
 
         void OnEnable() {
